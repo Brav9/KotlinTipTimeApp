2281b13c03c8f6d815aa62db5488277f5854e147
Brav9 <vskh93.009@gmail.com>
2022-08-21 18:10:21 +0300

    Добавление метода handleKeyEvent() для скрытия клавиатуры, при нажатии на Enter.

--- app/src/main/java/com/hfad/kotlintiptimeapp/MainActivity.kt
+++ app/src/main/java/com/hfad/kotlintiptimeapp/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.hfad.kotlintiptimeapp
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.KeyEvent.KEYCODE_ENTER
+import android.view.View
+import android.view.inputmethod.InputMethodManager
 import com.hfad.kotlintiptimeapp.databinding.ActivityMainBinding
 import java.text.NumberFormat
 
@@ -16,6 +20,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         binding.calculateButton.setOnClickListener { calculateTip() }
+        binding.costOfServiceEditText.setOnKeyListener { view, keyCode, _ -> handleKeyEvent(view, keyCode)}
     }
 
     private fun calculateTip() {
@@ -43,5 +48,15 @@ class MainActivity : AppCompatActivity() {
     private fun displayTip(tip: Double) {
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+
+    private fun handleKeyEvent(view: View, keyCode: Int): Boolean {
+        if (keyCode == KEYCODE_ENTER) {
+            val inputMethodManager =
+                getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
+            return true
+        }
+        return false
     }
 }
